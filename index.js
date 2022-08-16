fe07048c9ff3c2464e937d3233bf1f18e83f0a56
Evans Ochieng <evansochieng121@gmail.com>
2022-08-16 14:41:31 +0300

    Completed Phase1 assignment

--- index.js
+++ index.js
@@ -1,42 +1,88 @@
-// Write your solution here!
+// Phase 0
+// // Write your solution here!
+// const cats = ["Milo", "Otis", "Garfield"];
+
+// // Append element at the end of the array
+// function destructivelyAppendCat(name){
+//     return cats.push(name)
+// };
+
+// // Append element at the start of the array
+// function destructivelyPrependCat(name){
+//     return cats.unshift(name)
+// };
+
+// // Remove element at the end of the array
+// function destructivelyRemoveLastCat(name){
+//     return cats.pop()
+// };
+
+// // Remove element at the beginning of the array
+// function destructivelyRemoveFirstCat(name){
+//     return cats.shift()
+// };
+
+// // Undestructively add element at the end array
+// function appendCat(name){
+//     return [...cats, name]
+// };
+
+// // Undestructively add element at the start of the array
+// function prependCat(name){
+//     return [name, ...cats]
+// };
+
+// // Undestructively remove last element from the array
+// function removeLastCat(name){
+//     return cats.slice(0, -1)
+// };
+
+// // Undestructively remove first element from the array
+// function removeFirstCat(name){
+//     return cats.slice(1)
+// };
+
+
+// Phase 1
 const cats = ["Milo", "Otis", "Garfield"];
 
-// Append element at the end of the array
-function destructivelyAppendCat(name){
-    return cats.push(name)
+// Destructively append cat at the end of array
+//destructivelyAppendCat(name)
+function destructivelyAppendCat(cat) {
+    return cats.push(cat);
 };
 
-// Append element at the start of the array
-function destructivelyPrependCat(name){
-    return cats.unshift(name)
+// destructivelyPrependCat(name)
+function destructivelyPrependCat(cat) {
+    return cats.unshift(cat);
 };
 
-// Remove element at the end of the array
-function destructivelyRemoveLastCat(name){
-    return cats.pop()
+// destructivelyRemoveLastCat()
+function destructivelyRemoveLastCat(){
+    return cats.pop();
 };
 
-// Remove element at the beginning of the array
-function destructivelyRemoveFirstCat(name){
-    return cats.shift()
+// destructivelyRemoveFirstCat()
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
 };
 
-// Undestructively add element at the end array
-function appendCat(name){
-    return [...cats, name]
+// appendCat(name)
+function appendCat(cat){
+    return [...cats, cat];
 };
 
-// Undestructively add element at the start of the array
-function prependCat(name){
-    return [name, ...cats]
+// prependCat(name)
+function prependCat(cat){
+    return [cat, ...cats];
 };
 
-// Undestructively remove last element from the array
-function removeLastCat(name){
-    return cats.slice(0, -1)
+// removeLastCat()
+function removeLastCat(){
+    return cats.slice(0, -1);
 };
 
-// Undestructively remove first element from the array
-function removeFirstCat(name){
-    return cats.slice(1)
+// removeFirstCat()
+function removeFirstCat(){
+    return cats.slice(1);
 };
